621b2a9edb2123c7febf904b9e98934053f47d3c
Dan Groze <dangroze@gmail.com>
2019-02-18 17:13:15 +0000

    Passed test- note prints text

---
+++ src/note-model.js
@@ -0,0 +1,14 @@
+'use strict';
+
+(function(exports) {
+  function Note(text) {
+    this.text = text;
+  };
+
+  Note.prototype.printText = function(){
+    return this.text;
+  };
+
+  exports.Note = Note;
+
+})(this);
