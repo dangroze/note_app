8b77e6e99b8c15d0cc3a81370bd0c4e619025a6b
Dan Groze <dangroze@gmail.com>
2019-02-19 14:16:46 +0000

    Test - note list view

---
+++ spec/note-list-viewSpec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function testPrintListView() {
+  var noteListView = new NoteListView();
+  var noteList = new NoteList();
+  noteList.addToList('note');
+  assert.isTrue(noteListView.printNotes(noteList) === ('<div><li>' + 'note' + '</div></li>'));
+}
+testPrintListView();
